01e430e9af30f9c0f8fb02f2f71db07431fa1ccb
霗雫 <1287939667@qq.com>
2022-02-13 20:40:39 +0800

    增加代码

---
+++ code/输入.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int carrots;
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrots;                 //C++ input
+	cout << "Here are two more.";
+	carrots = carrots + 2;          //the next line concatenates output
+	cout << "Now you have " << carrots << " carrots." << endl;
+	return 0;
+}
