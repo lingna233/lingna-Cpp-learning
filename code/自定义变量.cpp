78adad78158d7010ba5ac6a9232769c545b0ed67
霗雫 <1287939667@qq.com>
2022-02-13 18:09:55 +0800

    添加项目文件。

---
+++ code/自定义变量.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int carrots;               //定义一个变量carrots
+	carrots = 25;              //将carrots赋值为25
+	cout << "I have ";         //C++ output
+	cout << carrots;
+	cout << " carrots.";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "Crunch, crunch.Now I have " << carrots << " carrots." << endl;
+	return 0;
+}
