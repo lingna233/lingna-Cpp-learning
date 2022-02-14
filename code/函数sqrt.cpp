917bb19223ce19971fcc0f9baea63ef725d2c91a
霗雫 <1287939667@qq.com>
2022-02-15 00:31:07 +0800

    学习中······

---
+++ code/函数sqrt.cpp
@@ -0,0 +1,18 @@
+/*
+*  º¯Êýsqrt.cpp -- using the sqrt() function
+*/
+#include <iostream>
+#include <cmath>     // or math.h
+using namespace std;
+int main()
+{
+	double area;
+	cout << "Enter the floor area,in square feet,of your home:";
+	cin >> area;
+	double side;
+	side = sqrt(area);
+	cout << "That's the eqivalent of a square " << side
+		 << " feet to the side." << endl;
+	cout << "How fascinating!" << endl;
+	return 0;
+}
