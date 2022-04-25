fc9ba6472ee91919e7d3bdf622cd10a9be530eb2
雫 霗 <lingna2233@outlook.com>
2022-04-25 14:16:28 +0800

    add code

---
+++ code/arraynew.cpp
@@ -0,0 +1,18 @@
+/*
+	arraynew.cpp -- using the operator for array
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	double* p3 = new double[3];        // space for 3 doubles
+	p3[0] = 0.2;                       // treat p3 like an array name
+	p3[1] = 0.5;
+	p3[2] = 0.8;
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 + 1;                       // increment the pointer
+	cout << "Now p3[0] is " << p3[0] << " and " << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 - 1;                       // point back to beginning
+	delete[] p3;
+	return 0;
+}
