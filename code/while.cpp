ee0559f9a1acfb445ae6b1a448c6ee2fb28d8058
雫 霗 <lingna2233@outlook.com>
2022-05-16 00:09:04 +0800

    add code

---
+++ code/while.cpp
@@ -0,0 +1,22 @@
+/*
+	while.cpp -- introducing the while loop
+*/
+#include <iostream>
+
+using namespace std;
+
+const int ArSize = 20;
+
+int main() {
+	char name[ArSize];
+	cout << "Your first name, please: ";
+	cin >> name;
+	cout << "Here is your name, verticalized and ASCIIized:\n";
+	int i = 0;                                // strat at beginning of string
+	while (name[i] != '\0') {                 // process to end of string
+ // while (name[i])
+		cout << name[i] << ": " << (int)name[i] << endl;
+		i++;                                  // don't forget this step
+	}
+	return 0;
+}
