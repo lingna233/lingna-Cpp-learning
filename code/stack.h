e758ed08a21a960618fc04a07b15a6f5b3f86de0
雫 霗 <lingna2233@outlook.com>
2022-10-03 17:30:01 +0800

    add code

---
+++ code/stack.h
@@ -0,0 +1,24 @@
+/*
+	stack.h -- class definition for the stack ADT
+*/
+#ifndef __STACK_H__
+#define __STACK_H__
+
+typedef unsigned long Item;		
+
+class Stack
+{
+private:
+	enum {MAX = 10};	// constant specific to class
+	Item items[MAX];	// holds stack items
+	int top;	// index for top stack item
+public:
+	Stack();
+	bool isempty() const;
+	bool isfull() const;
+	// push() return false if stack already is full, true otherwise
+	bool push(const Item &item);	// add item to stack
+	// pop() return false if stack already is empty, true otherwise
+	bool pop(Item &item);	// pop top into item
+};
+#endif
